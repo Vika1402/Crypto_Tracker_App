8ec449815bd9868f4e98f14f6289ced2636263d8
Vika1402 <vikas.kumar123@ssipmt.com>
2024-12-18 17:28:20 +0530

    start crypto api project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-[20px]">helo</div>
+      <button className="">ok</button>
+    </>
+  );
+}
+
+export default App;
