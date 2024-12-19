eb7750e3de0471f6e7da4e0e9e74b49d7d25d92a
Vika1402 <vikas.kumar123@ssipmt.com>
2024-12-19 22:01:58 +0530

    lazy loading and react suspenstion added in routing.jsx

--- src/Componenets/Routing/Routing.jsx
+++ src/Componenets/Routing/Routing.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "../../pages/Home";
-import CoinDetailsPage from "../../pages/CoinDetailsPage";
+import ContentLoader, { Facebook, Code } from "react-content-loader";
+// import Home from "../../pages/Home";
+// import CoinDetailsPage from "../../pages/CoinDetailsPage";
 import Layout from "../../pages/Layout";
-
+import { lazy } from "react";
+const Home = lazy(() => import("../../pages/Home"));
+const CoinDetailsPage = lazy(() => import("../../pages/CoinDetailsPage"));
 function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} /> /////index also use as '/'
-        <Route path="/details/:coinId" element=<CoinDetailsPage /> />
+        <Route
+          index
+          element={
+            <Suspense fallback={<Facebook />}>
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/details/:coinId"
+          element=<Suspense fallback={<Code />}>
+            <CoinDetailsPage />
+          </Suspense>
+        />
       </Route>
     </Routes>
   );
